94af293b6ba0df34a26ccbd3e91bbd7b140d624d
R4C3R0P <54683866+instax-dutta@users.noreply.github.com>
2022-09-03 18:20:19 +0530

    Create aegis.sh

---
+++ aegis.sh
@@ -0,0 +1,34 @@
+#Also, please take a look at our readme_aegis.txt, there are also useful firewall/iptables rules!
+
+iptables=/sbin/iptables
+
+# Which port should we protect against attacks?
+port=25565
+
+# Should we block linux connections? It can block a lot of attacks (with proxies)
+block_linux_connections=true
+
+limit_global_connections=true
+limit_global_connections_max=1
+
+# Limit new connections per second. This definitely helps aegis to stop massive attacks.
+burstconns=50
+
+$iptables -A INPUT -p tcp --dport $port --syn -m limit --limit $burstconns/s -j ACCEPT
+$iptables -A INPUT -p tcp --dport $port --syn -j DROP
+
+if $block_linux_connections ; then
+    $iptables -A INPUT -p tcp -m tcp --syn --tcp-option 8 --dport $port -j REJECT
+    echo 'Blocked linux connections!'
+fi
+
+if $limit_global_connections ; then
+    $iptables -I INPUT -p tcp --dport $port -m state --state NEW -m limit --limit $limit_global_connections_max/s -j ACCEPT
+    $iptables -A INPUT -p tcp -m tcp --dport $port -j ACCEPT
+    $iptables -A INPUT -p tcp -m tcp --dport $port -m state --state RELATED,ESTABLISHED -j ACCEPT
+    $iptables -A INPUT -p tcp -m tcp --dport $port --tcp-flags FIN,SYN,RST,ACK SYN -m connlimit --connlimit-above 150 --connlimit-mask 32 --connlimit-saddr -j DROP
+    $iptables -A INPUT -p tcp -m tcp --dport $port --tcp-flags FIN,SYN,RST,ACK SYN -m connlimit --connlimit-above 10 --connlimit-mask 32 --connlimit-saddr -j DROP
+    echo 'Limited global connections!'
+fi
+
+echo "Firewall applied successfully."
