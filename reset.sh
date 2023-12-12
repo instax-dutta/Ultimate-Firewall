976751c418b7a2ef33c588fa9026ecc31215720c
R4C3R0P <54683866+instax-dutta@users.noreply.github.com>
2023-12-12 20:42:44 +0530

    updated ;)

--- reset.sh
+++ reset.sh
@@ -1,20 +1,32 @@
-echo "Resetting the Ip Table Rules"
-wait for 5 seconds
+#!/bin/bash
+
+echo "Resetting the IP Table Rules"
+sleep 5
+
+# Set default policies to ACCEPT
 iptables -P INPUT ACCEPT
 iptables -P OUTPUT ACCEPT
 iptables -P FORWARD ACCEPT
-wait for 5 seconds
+
+sleep 5
+
 echo "Deleting the Existing Rules"
-wait for 5 seconds
+sleep 5
+
+# Flush all existing rules
 iptables -F INPUT
 iptables -F OUTPUT
 iptables -F FORWARD
-wait for 5 seconds
-echo "Deleting the Existing Chains"
-iptables -F
-wait for 2 seconds
 
-echo "Done Resetting the Ip Table Rules"
+sleep 5
+
+echo "Deleting the Existing Chains"
+# Delete any custom chains
+iptables -X
+
+sleep 2
+
+echo "Done Resetting the IP Table Rules"
 echo "Exiting the Script"
 echo "We suggest a reboot to make sure the rules are reset"
 exit 0
